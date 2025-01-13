a53e38ff9c0c91cbd860ef4648fe8ec479d259b0
Shekhar <shekhar@Shekhars-Laptop.local>
2025-01-13 15:03:47 +0530

    spring initializr

---
+++ src/main/java/com/example/ReusableAuthService/ReusableAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReusableAuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReusableAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReusableAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReusableAuthService/ReusableAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReusableAuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReusableAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
